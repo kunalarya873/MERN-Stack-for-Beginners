2ad5e9dbe04b8a5f130a5de4035b7200fb84b3d3
kunalarya873 <kunalarya873@gmail.com>
2024-04-09 14:28:42 +0530

    Adding all basic modules

--- NodeJS/module.js
+++ NodeJS/module.js
@@ -30,4 +30,43 @@ let os = require('os');
 let fs = require('fs');
 fs.writeFileSync('test.txt', 'Hello Testing');
 fs.appendFileSync('test.txt', 'Append');
-console.log(fs.readFileSync('./test.txt').toString());
+// console.log(fs.readFileSync('./test.txt').toString());
+//fs.unlinkSync('./test.txt'); for deleting the file
+//file Cration other option
+fs.writeFile('test2.txt', 'Second file testing', function(error){
+    if(!error){
+        // console.log('File created succesfuly');
+    }
+});
+
+
+
+//event module
+//use to perform the module
+
+const { EventEmitter }  = require('events');
+const eventEmitter = new EventEmitter();
+eventEmitter.on('event-1', function(param1){
+    // console.log('hi there');
+    // console.log(param1);
+});
+
+// eventEmitter.emit('event-1', 'bye there');
+
+
+
+//http module
+
+let http = require('http');
+let server = http.createServer((request, response) => {
+    if(request.url === '/about'){
+        response.write('This is home page');
+    }
+    response.end();
+});
+server.on('connection', function(){
+    console.log('Connecting...');
+});
+server.listen(3000, () => {
+    console.log('Server is running');
+});
--- NodeJS/tempCodeRunnerFile.js
+++ NodeJS/tempCodeRunnerFile.js
@@ -1,1 +1,11 @@
-console.log(fs.readFileSync().toString());
+
+const events = require('events');
+const { EventEmmitter } = events;
+const eventEmitter = new EventEmmitter();
+
+eventEmitter.on('event-1', function(param1){
+    console.log('hi there');
+    console.log(param1);
+});
+
+eventEmitter.emit('event-1', 'bye there');
